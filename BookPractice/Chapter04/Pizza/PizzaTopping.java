0ebe0374e1802c18952299bc48f0421a5a896615
LaiTial <39575609+LaiTial@users.noreply.github.com>
2019-09-05 23:09:12 +0900

    Create PizzaTopping.java

---
+++ BookPractice/Chapter04/Pizza/PizzaTopping.java
@@ -0,0 +1,22 @@
+/*
+2019-02-04
+어서와 Java는 처음이지!
+chapter4
+배열의 toString 함수 연습 프로그램
+*/
+
+package out_pr;
+
+import java.util.Arrays;
+
+public class PizzaTopping {
+
+	public static void main(String[] args) {
+		
+
+		String[] food = {"Pepperoni", "Mushrooms", "Onions", "Sausage", "Bacon"};
+		
+		System.out.println(Arrays.toString(food));
+	}
+
+}
