51bcc97d2d71247f308dac63488c28b85024464d
LaiTial <39575609+LaiTial@users.noreply.github.com>
2019-09-05 23:04:27 +0900

    Create GetMin.java

---
+++ BookPractice/Chapter04/GetMin/GetMin.java
@@ -0,0 +1,28 @@
+/*
+2019-02-04
+어서와 Java는 처음이지!
+chapter3
+10개의 배열에 저장된 값들 중에서 최소값을 찾아내는 프로그램
+*/
+
+package find_tw;
+
+public class getMin {
+
+	public static void main(String[] args) {
+		
+		int[] s = {12, 3, 19, 6, 18, 22, 11, 7, 17, 5};
+		int temp = 100;
+		
+		for(int i=0; i< s.length; i++)
+		{
+			if(s[i] < temp)
+			{
+				temp = s[i];
+			}
+		}
+		
+		System.out.println("최소값은 " + temp + "입니다.");
+	}
+
+}
