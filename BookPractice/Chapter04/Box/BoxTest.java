11ffab2ff25b8ba6e1aaa95626d5f063dc22a3c2
LaiTial <39575609+LaiTial@users.noreply.github.com>
2019-09-05 23:25:29 +0900

    Create BoxTest.java

---
+++ BookPractice/Chapter04/Box/BoxTest.java
@@ -0,0 +1,24 @@
+*
+2019-02-10
+어서와 Java는 처음이지!
+chapter4
+상자의 가로, 세로, 높이를 맴버로 갖는 객체를 만들어 
+main 함수에서 호출하고 처리하는 프로그램
+*/
+
+package test;
+
+public class BoxTest {
+
+	public static void main(String[] args) {
+		
+		Box cx = new Box();
+		
+		cx.height = 30;
+		cx.length = 20;
+		cx.width = 20;
+		
+		System.out.println("상자의 가로, 세로, 높이는 "+ cx.width + ", " + cx.length + ", " + cx.height + "입니다." );
+	}
+
+}
