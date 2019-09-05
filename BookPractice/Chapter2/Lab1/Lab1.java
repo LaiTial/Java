c20ed36008c443a72be4014556e761543ac96695
LaiTial <39575609+LaiTial@users.noreply.github.com>
2019-09-05 22:21:56 +0900

    Add files via upload

---
+++ BookPractice/Chapter2/Lab1/Lab1.java
@@ -0,0 +1,11 @@
+package lab;
+
+public class Lab1 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+		System.out.println("I'm a new Java Programmer.");
+	}
+
+}
