878ea0b569cb1879d8e78cadda40622e836a1fd5
LaiTial <39575609+LaiTial@users.noreply.github.com>
2019-09-05 23:37:32 +0900

    Add files via upload

---
+++ BookPractice/Chapter06/Box/BoxTest.java
@@ -0,0 +1,13 @@
+package test;
+
+public class BoxTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Box cx = new Box(4, 5, 8);
+		
+		System.out.println("상자의 부피는 " + cx.getVolume() + "입니다.");
+	}
+
+}
