f70ad94eaa61cfc3c53ae07db500513aa89bff99
LaiTial <39575609+LaiTial@users.noreply.github.com>
2019-09-05 23:47:06 +0900

    Add files via upload

---
+++ BookPractice/Chapter06/Television/Television.java
@@ -0,0 +1,21 @@
+package test1;
+
+public class Television {
+	
+	private int channel;
+	private int volume;
+	private boolean onOff;
+	
+	Television(int channel, int volume, boolean onOff) {
+		
+		this.channel = channel;
+		this.volume = volume;
+		this.onOff = onOff;
+	}
+	
+	void print() {
+		
+		System.out.println("채널은 " + channel + "이고 볼륨은 " + volume + "입니다.");
+	}
+
+}
---
+++ BookPractice/Chapter06/Television/TelevisionTest.java
@@ -0,0 +1,13 @@
+package test1;
+
+public class TelevisionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Television cx = new Television(34, 70, true);
+		
+		cx.print();
+	}
+
+}
