d08b2a813e3d2d6e6cb20704dd769165773b3b17
LaiTial <39575609+LaiTial@users.noreply.github.com>
2019-09-09 22:15:51 +0900

    Add files via upload

---
+++ BookPractice/Chapter09/Comparable_check.java
@@ -0,0 +1,21 @@
+
+public class Comparable_check {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Rectangle n1 = new Rectangle(100, 30);
+		Rectangle n2 = new Rectangle(200, 10);
+		
+
+		if(n1.compareTo(n2) > 0)
+			System.out.println(n1.toString() + "가 더 큽니다.");
+		
+		else if(n1.compareTo(n2) == 0)
+			System.out.println("둘은 같습니다.");
+		
+		else
+			System.out.println(n2.toString() + "가 더 큽니다.");
+	}
+
+}
