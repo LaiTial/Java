8aaff822d1185c12c24b69c1693875e7790c16d3
LaiTial <39575609+LaiTial@users.noreply.github.com>
2019-09-09 22:16:27 +0900

    Update and rename BookPractice/Chapter09/Comparable_check.java to BookPractice/Chapter09/Comparable/Comparable_check.java

--- BookPractice/Chapter09/Comparable/Comparable_check.java
+++ BookPractice/Chapter09/Comparable/Comparable_check.java
@@ -2,20 +2,19 @@
 public class Comparable_check {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 		Rectangle n1 = new Rectangle(100, 30);
 		Rectangle n2 = new Rectangle(200, 10);
 		
 
 		if(n1.compareTo(n2) > 0)
-			System.out.println(n1.toString() + "°¡ ´õ Å®´Ï´Ù.");
+			System.out.println(n1.toString() + "ê°€ ë” í½ë‹ˆë‹¤.");
 		
 		else if(n1.compareTo(n2) == 0)
-			System.out.println("µÑÀº °°½À´Ï´Ù.");
+			System.out.println("ë‘˜ì€ ê°™ìŠµë‹ˆë‹¤.");
 		
 		else
-			System.out.println(n2.toString() + "°¡ ´õ Å®´Ï´Ù.");
+			System.out.println(n2.toString() + "ê°€ ë” í½ë‹ˆë‹¤.");
 	}
 
 }
