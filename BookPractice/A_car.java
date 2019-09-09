660dd8e3394e53f9f64bd16c2dfdcf3d9c901f66
LaiTial <39575609+LaiTial@users.noreply.github.com>
2019-09-09 22:14:22 +0900

    Add files via upload

---
+++ BookPractice/A_car.java
@@ -0,0 +1,46 @@
+
+public class A_car implements Make_car_in{
+
+	private int speed;
+	private int degree;
+	
+	@Override
+	public void start()
+	{
+		System.out.println("자동차가 출발합니다.");
+	}
+	
+	@Override
+	public void stop()
+	{
+		System.out.println("자동차가 정지합니다.");
+	}
+	
+	@Override
+	public int setSpeed(int speed)
+	{
+		this.speed = speed;
+		System.out.println("자동차가 속도를 " + speed + "km/h로 바꿉니다.");
+		return speed;
+	}
+	
+	@Override
+	public int turn(int degree)
+	{
+		this.degree = degree;
+		System.out.println("자동차가 방향을 " + degree + "도 만큼 바꿉니다.");
+		return degree;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		A_car car = new A_car();
+		
+		car.start();
+		car.setSpeed(30);
+		car.turn(15);
+		car.stop();
+	}
+
+}
